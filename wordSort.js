592fc4ca0779cd81344b82e14fdc0e695eea7eb2
jennclee <8753352+jennclee@users.noreply.github.com>
2018-03-08 15:14:46 -0800

    Initial commit for wordSort toy problem

---
+++ wordSort.js
@@ -0,0 +1,16 @@
+/*
+Given two input arrays, return true if the words array is sorted according to the ordering array
+Input:
+words = ['cc', 'cb', 'bb', 'ac']
+ordering = ['c', 'b', 'a']
+Output: True
+
+Input:
+words = ['cc', 'cb', 'bb', 'ac']
+ordering = ['b', 'c', 'a']
+Output: False
+*/
+
+const isArraySorted = (array, ordering) => {
+
+};
