f5fa5fe083ee269413397b2bcc4c80002a061cb3
jennclee <8753352+jennclee@users.noreply.github.com>
2018-03-09 17:45:13 -0800

    Pseudocode of strategy for findPalindromePairs

--- findPalindromePairs.js
+++ findPalindromePairs.js
@@ -5,5 +5,15 @@ Given a dictionary, find all pairs of words that, when concatenated together, fo
 */
 
 const findPalindromePairs = (dictionary) => {
-
+  // have array for storing successful wordPairs
+  // iterate through dictionary
+    // retrieve two words each time
+    // prepend and append first word to second word
+    // for both prepended and appended words
+      // firstHalf = word.slice(0,word.length/2).split();
+      // secondHalf = word.slice(word.length/2, word.length).split();
+      // for each character in firstHalf.reverse()
+        // if firstHalf.reverse()[i] === secondHalf[i] && i === firstHalf.reverse().length
+          // add pair to wordPairs array
+  // return wordPairs
 };
