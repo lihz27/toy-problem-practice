cd6e1f187817598d678399357e6c6fec8cc5c83c
jennclee <8753352+jennclee@users.noreply.github.com>
2018-03-09 17:43:55 -0800

    Initial commit for findPalindromePairs

---
+++ findPalindromePairs.js
@@ -0,0 +1,9 @@
+/*
+Given a dictionary, find all pairs of words that, when concatenated together, form a palindrome.
+‘none', 'xenon': 'nonexenon' is a palindrome
+'none', 'xexenon': 'nonexexenon' is a palindrome
+*/
+
+const findPalindromePairs = (dictionary) => {
+
+};
