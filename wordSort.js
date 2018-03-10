052cc37c8f9ed7b30e2de6191c4b6ead587d697c
jennclee <8753352+jennclee@users.noreply.github.com>
2018-03-09 16:05:56 -0800

    O(n) solution for wordSort

--- wordSort.js
+++ wordSort.js
@@ -11,6 +11,27 @@ ordering = ['b', 'c', 'a']
 Output: False
 */
 
-const isArraySorted = (array, ordering) => {
-
+const isArraySorted = (wordList, orderList) => {
+  // keep track of currentLetter encountered in orderList
+  // iterate through wordList
+  // if nextLetter in wordList is not the same as currentLetter
+    // check if nextLetter === currentLetter + 1
+      // if true, currentLetter = orderList[currentLetterIdx + 1]
+      // if false, return false
+  let tempIndex, orderIndex = 0;
+  let currentLetter = orderList[orderIndex];
+  for (let i = 0; i < wordList.length; i++) {
+    if (wordList[i][0] !== currentLetter && orderIndex !== orderList.length - 1){
+      currentLetter = orderList[++orderIndex];
+      if (wordList[i][0] !== currentLetter) {
+        return false;
+      }
+    } else if (orderIndex === orderList.length - 1){
+      return false;
+    }
+  }
+  return true;
 };
+
+console.log(isArraySorted(['cc', 'cb', 'bb', 'ac'], ['c', 'b', 'a']));
+console.log(isArraySorted(['cc', 'cb', 'bb', 'ac'], ['b', 'c', 'a']));
