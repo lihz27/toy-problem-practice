ba3c4a4daf86eacd8359cdfa7197a221197b4577
jennclee <8753352+jennclee@users.noreply.github.com>
2018-03-09 18:21:26 -0800

    Update examples for findPalindromePairs

--- findPalindromePairs.js
+++ findPalindromePairs.js
@@ -1,7 +1,8 @@
 /*
 Given a dictionary, find all pairs of words that, when concatenated together, form a palindrome.
-‘none', 'xenon': 'nonexenon' is a palindrome
-'none', 'xexenon': 'nonexexenon' is a palindrome
+‘none', 'xenon': 'nonexenon'
+'amore', 'roma': 'amoreroma'
+'taco', 'cat': 'tacocat'
 */
 
 const findPalindromePairs = (dictionary) => {
