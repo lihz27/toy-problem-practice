34575875cb28ce8a988ae9e31867aefe3ab1893e
jennclee <8753352+jennclee@users.noreply.github.com>
2018-03-09 17:44:32 -0800

    Initial commit for wordPattern

---
+++ wordPattern.js
@@ -0,0 +1,10 @@
+/*
+Given list of strings and a pattern, such as *an* where * can match any number of chracters, return matching words in an efficient manner.
+
+Input: ['crane', 'drain', 'refrain'], *an*
+Output: 'crane'
+*/
+
+const wordPattern = (words, pattern) => {
+
+};
