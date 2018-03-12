7bac603460c5e3cb2a43200b3d21efba67409862
jennclee <8753352+jennclee@users.noreply.github.com>
2018-03-12 15:46:13 -0700

    Initial commit for prompts

---
+++ anagramServer.js
@@ -0,0 +1,12 @@
+/*
+Given a list of valid English words, build an anagram server that can spit out all the anagrams of a word.
+
+(anagram = permutation that is also valid word)
+rates = aster, stare, tears, tares, taser
+dgo = dog, god
+aeelps = please, elapse, asleep
+*/
+
+const anagramServer = (word, wordList) => {
+
+};
---
+++ commonArrayElements.js
@@ -0,0 +1,10 @@
+/*
+Given two arrays that are sorted and distinct, find the number of elements in common.
+
+Input: [1, 3, 7, 21], [2, 3, 5, 7, 10, 16, 21, 23]
+Output: 2
+*/
+
+const commonArrayElements = (array1, array2) => {
+
+};
