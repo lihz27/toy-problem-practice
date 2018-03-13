7ae88c7304ef308c3e22764ebe3feeda3cbccdf7
jennclee <8753352+jennclee@users.noreply.github.com>
2018-03-12 21:17:17 -0700

    O(n) solution for twoSum

--- twoSum.js
+++ twoSum.js
@@ -11,5 +11,26 @@ return [0, 1].
 */
 
 const twoSum = (nums, target) => {
-
+  /*  
+    for (let i = 0; i < nums.length; i++) {
+      for (let j = i + 1; j < nums.length; j++) {
+        if (target === nums[i] + nums[j]) {
+          return [i,j];
+        }
+      }
+    }
+    return undefined;
+    // Solution is in O(n^2). Takes care of some negative numbers in nums array
+  */
+  const hash = {};
+  for (let i = 0; i < nums.length; i++) {
+    hash[nums[i]] = i;
+  }
+  for (let i = 0; i < nums.length; i++) {
+    const leftover = target - nums[i];
+    if (hash[leftover]) {
+      return [i, hash[leftover]];
+    }
+  }
+  return [];
 };
