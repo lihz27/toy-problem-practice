3db39e991de6f71207e68ff7fc7d25ac7419c78d
jennclee <8753352+jennclee@users.noreply.github.com>
2018-03-19 16:59:24 -0700

    Initial prompt commit

---
+++ maxEvenPathSum.js
@@ -0,0 +1,13 @@
+/*
+Given a binary tree, output the maximum EVEN sum along any path
+      10
+     /    \
+    2      5
+   /  \      \
+  1   101    13
+Maximum even sum = 101 +2 +10 + 5 = 118
+*/
+
+const maxEvenSum = (tree) => {
+  
+}
