e5d6b999cbeb6e3be75dc3ad5cd6cbc5c76b4ebc
jennclee <8753352+jennclee@users.noreply.github.com>
2018-03-20 14:03:51 -0700

    Update prompt

--- ctci/leftRotation.js
+++ ctci/leftRotation.js
@@ -1,12 +1,15 @@
 /*
-A left rotation operation on an array of size  shifts each of the array's elements  unit to the left. For example, if  left rotations are performed on array , then the array would become .
+A left rotation operation on an array of size n shifts each of the array's elements 1 unit to the left. For example, if 2 left rotations are performed on array [1, 2, 3, 4, 5], then the array would become [3, 4, 5, 1, 2].
 
-Given an array of  integers and a number, , perform  left rotations on the array. Then print the updated array as a single line of space-separated integers.
+Given an array of n integers and a number, d, perform d left rotations on the array and return the array.
 
 Example:
 leftRotation([1, 2, 3, 4, 5], 4);  // [5, 1, 2, 3, 4]
 */
 
 const leftRotation = (array, rotation) => {
+  if (array.length % rotation === 0) {
+    return array;
+  }
 
 };
