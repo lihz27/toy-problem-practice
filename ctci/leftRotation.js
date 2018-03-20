18753c32ab0bdf10ccf8fa55f2e7998f880b73ca
jennclee <8753352+jennclee@users.noreply.github.com>
2018-03-20 14:52:08 -0700

    O(n) solution to leftRotation

--- ctci/leftRotation.js
+++ ctci/leftRotation.js
@@ -4,12 +4,22 @@ A left rotation operation on an array of size n shifts each of the array's eleme
 Given an array of n integers and a number, d, perform d left rotations on the array and return the array.
 
 Example:
+leftRotation([1, 2, 3, 4, 5], 2);  // [3, 4, 5, 1, 2]
 leftRotation([1, 2, 3, 4, 5], 4);  // [5, 1, 2, 3, 4]
 */
 
 const leftRotation = (array, rotation) => {
-  if (array.length % rotation === 0) {
+  if (rotation === null || array.length === 0) {
+    return null;
+  }
+  if (rotation % array.length === 0) {
     return array;
   }
-
+  const indexShift = array.length - rotation;
+  const rotatedArray = [];
+  for (let i = 0; i < array.length; i++) {
+    const newIndex = (i + indexShift) % array.length;
+    rotatedArray[newIndex] = array[i];
+  }
+  return rotatedArray;
 };
