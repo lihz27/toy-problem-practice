035c50273ca651b73412077d83140ea5fac40bd8
jennclee <8753352+jennclee@users.noreply.github.com>
2018-03-26 16:21:50 -0700

    Initial prompt commit

---
+++ ctci/data-structures/ransomNote.js
@@ -0,0 +1,16 @@
+/*
+A kidnapper wrote a ransom note but is worried it will be traced back to him. He found a magazine and wants to know if he can cut out whole words from it and use them to create an untraceable replica of his ransom note. The words in his note are case-sensitive and he must use whole words available in the magazine, meaning he cannot use substrings or concatenation to create the words he needs.
+
+Given the words in the magazine and the words in the ransom note, print Yes if he can replicate his ransom note exactly using whole words from the magazine; otherwise, print No.
+
+NOTES:
+- Each word consists of English alphabetic letters (i.e., a to z and A to Z).
+- The words in the note and magazine are case-sensitive.
+
+ransomNote('give me one grand today night', 'give one grand today');  // yes
+ransomNote('two times three is not four', 'two times two is four');   // no
+*/
+
+const ransomNote = (mag, note) => {
+
+};
