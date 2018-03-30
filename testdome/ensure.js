79480d9f555bd87f747fcf293004b3178fd881ad
jennclee <8753352+jennclee@users.noreply.github.com>
2018-03-29 20:07:22 -0700

    Add implementation of ensure function

---
+++ testdome/ensure.js
@@ -0,0 +1,10 @@
+/*
+Implement the ensure function so that it throws an error if called without arguments or the argument is undefined. Otherwise it should return the given value.
+*/
+
+function ensure(value) {
+  if (value === null || value === undefined) {
+    throw new Error('No arguments');
+  }
+  return value;
+}
