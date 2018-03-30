a260e2618cd30c22edd950e6f2755af5e4fa04a2
jennclee <8753352+jennclee@users.noreply.github.com>
2018-03-29 21:10:51 -0700

    Implement arrow function

--- testdome/removeProperty.js
+++ testdome/removeProperty.js
@@ -4,10 +4,10 @@ Implement the removeProperty function which takes an object and property name, a
 If the object obj has a property prop, the function removes the property from the object and returns true; in all other cases it returns false.
 */
 
-function removeProperty(obj, prop) {
+const removeProperty = (obj, prop) => {
   if (obj[prop] !== undefined) {
     delete obj[prop];
     return true;
   }
   return false;
-}
+};
