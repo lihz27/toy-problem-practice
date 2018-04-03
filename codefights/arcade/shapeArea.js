c074e44e5d46a087ce0352b9010204b9999e4b01
jennclee <8753352+jennclee@users.noreply.github.com>
2018-04-03 16:46:56 -0700

    Implement solution for shapeArea

--- codefights/arcade/shapeArea.js
+++ codefights/arcade/shapeArea.js
@@ -13,11 +13,13 @@ n = 1   n = 2     n = 3         n = 4
                                   □
 
 Example:
-For n = 2, the output should be shapeArea(n) = 5.
-For n = 3, the output should be shapeArea(n) = 13.
-For n = 4, the output should be shapeArea(n) = 25.
+For n = 2, the output should be shapeArea(n) = 5.  2^2 + 1 -> 2^2 + 1^2
+For n = 3, the output should be shapeArea(n) = 13. 3^2 + 4 -> 3^2 + 2^2
+For n = 4, the output should be shapeArea(n) = 25. 4^2 + 9 -> 4^2 + 3^2
 */
 
 const shapeArea = (n) => {
-
+  // pattern is n^2 + (n-1)^2 to get area
+  return (Math.pow(n, 2) + Math.pow(n - 1, 2));
 };
+
