7c9f02dd73f2934cb4b426513cbe1d15cefa9673
jennclee <8753352+jennclee@users.noreply.github.com>
2018-04-03 16:59:06 -0700

    Implement basic solution to makeArrayConsecutive

---
+++ codefights/arcade/makeArrayConsecutive2.js
@@ -0,0 +1,14 @@
+/*
+Ratiorg got statues of different sizes as a present from CodeMaster for his birthday, each statue having an non-negative integer size. Since he likes to make things perfect, he wants to arrange them from smallest to largest so that each statue will be bigger than the previous one exactly by 1. He may need some additional statues to be able to accomplish that. Help him figure out the minimum number of additional statues needed.
+
+Example
+statues = [6, 2, 3, 8]
+makeArrayConsecutive2(statues); // 3. Ratiorg needs statues of sizes 4, 5 and 7.
+*/
+
+// This function assumes array elements are unique
+const makeArrayConsecutive2 = (array) => {
+  const min = Math.min(...array);
+  const max = Math.max(...array);
+  return (max - min + 1) - array.length;
+};
