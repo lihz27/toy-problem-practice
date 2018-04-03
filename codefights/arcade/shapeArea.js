0ae83f2dc303d0a821428f8c10f0de81681fd36d
jennclee <8753352+jennclee@users.noreply.github.com>
2018-04-03 16:40:54 -0700

    Initial prompt commit

---
+++ codefights/arcade/shapeArea.js
@@ -0,0 +1,23 @@
+/*
+Below we will define an n-interesting polygon. Your task is to find the area of a polygon for a given n.
+
+A 1-interesting polygon is just a square with a side of length 1. An n-interesting polygon is obtained by taking the n - 1-interesting polygon and appending 1-interesting polygons to its rim, side by side.
+
+n = 1   n = 2     n = 3         n = 4
+ □        □         □             □
+        □ □ □     □ □ □         □ □ □
+          □     □ □ □ □ □     □ □ □ □ □
+                  □ □ □     □ □ □ □ □ □ □
+                    □         □ □ □ □ □
+                                □ □ □
+                                  □
+
+Example:
+For n = 2, the output should be shapeArea(n) = 5.
+For n = 3, the output should be shapeArea(n) = 13.
+For n = 4, the output should be shapeArea(n) = 25.
+*/
+
+const shapeArea = (n) => {
+
+};
