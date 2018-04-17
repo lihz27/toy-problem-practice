be3fb8a49ec0e205934415dcb97fe83f5c057bc9
jennclee <8753352+jennclee@users.noreply.github.com>
2018-04-17 16:21:14 -0700

    Solution to reverseString

---
+++ reverseString.js
@@ -0,0 +1,11 @@
+/*
+Write a function that takes a string as input and returns the string reversed.
+
+Example:
+Given s = "hello", return "olleh".
+*/
+
+var reverseString = function (s) {
+  var split = s.split('');
+  return split.reverse().join('');
+};
