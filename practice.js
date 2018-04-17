ebf07db31de5c5e57dea2df2a243e6ba514497b7
jennclee <8753352+jennclee@users.noreply.github.com>
2018-04-16 22:48:42 -0700

    Simple solution with assumptions

--- practice.js
+++ practice.js
@@ -10,7 +10,37 @@
 const aliases = ['John A. Smith', 'John Smith', 'John Adam Smith', 'Robert Adam Jones', 'Robert Jones'];
 
 const matchAlias = (name) => {
-
+  const aliasStore = [];
+  const nameSplit = name.split(' ');
+  const first = nameSplit[0];
+  let middle = null;
+  let last = null;
+  if (nameSplit.length === 2) {
+    last = nameSplit[1];
+  } else if (nameSplit.length === 3) {
+    middle = nameSplit[1];
+    last = nameSplit[2];
+  }
+  for (let i = 0; i < aliases.length; i++) {
+    const aliasSplit = aliases[i].split(' ');
+    const aFirst = aliasSplit[0];
+    let aMiddle = null;
+    let aLast = null;
+    if (aliasSplit.length === 2) {
+      aLast = aliasSplit[1];
+    } else if (aliasSplit.length === 3) {
+      aMiddle = aliasSplit[1];
+      aLast = aliasSplit[2];
+    }
+    if (aFirst !== first) {
+      break;
+    } else if (last === aLast || last[0] === aLast[0]) {
+      if (aMiddle === null || middle === null || middle === aMiddle || middle[0] === aMiddle[0]) {
+        aliasStore.push(aliases[i])
+      }
+    }
+  }
+  return aliasStore;
 }
 
 console.log(matchAlias('John Adam Smith'));
