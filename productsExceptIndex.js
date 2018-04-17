b022106322bf2df5ab9b821547ef14bbea3289cd
jennclee <8753352+jennclee@users.noreply.github.com>
2018-04-16 22:05:44 -0700

    Init prompt commit

---
+++ productsExceptIndex.js
@@ -0,0 +1,18 @@
+/*
+Write a function productsExceptIndex() that takes an array of integers and returns an array of the products.
+
+For example, given:
+  [1, 7, 3, 4]
+
+your function would return:
+  [84, 12, 28, 21]
+
+by calculating:
+  [7 * 3 * 4,  1 * 3 * 4,  1 * 7 * 4,  1 * 7 * 3]
+
+NOTE: Do not use division in your solution.
+*/
+
+const productsExceptIndex = (array) => {
+
+};
