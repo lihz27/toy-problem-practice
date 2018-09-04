446f405f2b0c259e130866325af02f236297526a
jennclee <8753352+jennclee@users.noreply.github.com>
2018-09-04 13:40:44 -0700

    Update to ES6 and use forEach method

--- balancedParens.js
+++ balancedParens.js
@@ -9,35 +9,33 @@ var parensArr = ['{[()]}', '{[(])}', '{{[[(())]]}}']
 balancedParens(parensArr);    // returns ['y', 'n', 'y']
 */
 
-function balancedParens(values) {
+const balancedParens = (values) => {
   var sol = [];
   var braces = {
     '{': '}',
     '(': ')',
     '[': ']'
   }
-  for (var i = 0; i < values.length; i++) {
-    if (values[i].length < 2) {
+  values.forEach((string) => {
+    if (string.length < 2) {
       temp = 'n';
     }
-    var split = values[i].split('');
+    var split = string.split('');
     var stack = [];
     var temp = 'y';
-    for (var j = 0; j < split.length; j++) {
-      if (braces[split[j]]) {
-        stack.push(split[j]);
-      } else if (split[j] === '}' || split[j] === ')' || split[j] === ']') {
-        if (braces[stack.pop()] !== split[j]) {
+    split.forEach((parens) => {
+      if (braces[parens]) {
+        stack.push(parens);
+      } else if (parens === '}' || parens === ')' || parens === ']') {
+        if (braces[stack.pop()] !== parens) {
           temp = 'n';
-          break;
-        } else {
         }
       }
-    }
+    })
     if (stack.length !== 0) {
       temp = 'n';
     }
     sol.push(temp);
-  }
+  })
   return sol;
 }
